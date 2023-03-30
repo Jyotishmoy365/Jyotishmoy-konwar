bd9e48c8058c9a3d50109c85489adab23f1ac6a2
Jyotishmoy Konwar <89648370+Jyotishmoy365@users.noreply.github.com>
2023-03-30 17:57:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+function green() {
+    let w = document.getElementById('dark')
+    w.style.backgroundColor = '#23241c';
+    w.style.color = '#949689';
+
+    let x = document.getElementById('drop')
+    x.style.display = 'none'
+
+
+    let z = document.getElementById('text');
+    z.style.color = 'green';
+}
+
+function drop() {
+    let a = document.getElementById('drop');
+    a.style.display = "block";
+}
+function brown() {
+    let x = document.getElementById('dark')
+    x.style.backgroundColor = '#2c2220';
+    x.style.color = '#9e8c88';
+    let y = document.getElementById('drop')
+    y.style.display = 'none'
+}
+function blue() {
+    let x = document.getElementById('dark')
+    x.style.backgroundColor = '#0b2748';
+    x.style.color = '#6da0dc';
+    let y = document.getElementById('drop')
+    y.style.display = 'none'
+}
+function pink() {
+    let x = document.getElementById('dark')
+    x.style.backgroundColor = '#310215';
+    x.style.color = '#f573a6';
+    let y = document.getElementById('drop')
+    y.style.display = 'none'
+}
